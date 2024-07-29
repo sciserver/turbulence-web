55594892ad61b82c3951a6dcb6d4e85e7bd156c5
Juliana Jaime <julianajaimej@gmail.com>
2024-07-29 19:20:46 -0400

    Removed points animation

--- components/content/pointsQueried.tsx
+++ components/content/pointsQueried.tsx
@@ -1,25 +1,13 @@
-// eslint-disable-next-line eslint-comments/disable-enable-pair
-/* eslint-disable import/no-extraneous-dependencies */
 import { FC, useMemo } from 'react';
-import dynamic from 'next/dynamic';
 import { useQuery } from '@apollo/client';
 import styled from 'styled-components';
-const AnimatedNumbers = dynamic(() => import('react-animated-numbers'), { ssr: false });
 
 import { POINTS_QUERIED } from 'src/graphql/points';
 
 
 const Styled = styled.div`
   .points {
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
     text-align: right;
-    gap: 15px;
-  }
-
-  .label {
-    font-size: 20px;
   }
 `;
 
@@ -36,22 +24,7 @@ export const PointsQueried: FC = () => {
   return <Styled>
     {points &&
       <div className="points">
-        <AnimatedNumbers
-          includeComma
-          locale="en-US"
-          animateToNumber={points}
-          className="points-font"
-          transitions={() => ({
-            type: 'spring',
-            duration: 1
-          })}
-          fontStyle={{
-            fontSize: 20,
-            fontFamily: 'Roboto Slab',
-            fontWeight: 600
-          }}
-        />
-        <h3 className="label">points queried</h3>
+        <h3> {points.toLocaleString()} points queried</h3>
       </div>
     }
   </Styled>;
